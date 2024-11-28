c20064593088c5f43b198fa059f83165ac79be72
jclipa <jclipanovich@gmail.com>
2024-11-27 21:18:34 -0300

    remove db config password

--- config.py
+++ config.py
@@ -9,6 +9,5 @@ JSONBIN_URL_VENTA_DETALLE = 'https://api.jsonbin.io/v3/b/6741dec2e41b4d34e4593be
 db_config = {
     'host': 'localhost',
     'user': 'root',  
-    'password': '', 
     'database': 'empresa_quimica'
 }
