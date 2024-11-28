e1ab8c61dbd21e8f0f5001b240c362b8dd4475ca
Nicodan122 <nicolas.dangelo@davinci.edu.ar>
2024-11-27 21:13:19 -0300

    datos-tabla

--- database/create.sql
+++ database/create.sql
@@ -8,3 +8,16 @@ CREATE TABLE wishlist (
     KEY (usuario_id),
     KEY (producto_id)
 );
+
+INSERT INTO wishlist (id, usuario_id, producto_id) VALUES
+(5, 2, 1),
+(3, 2, 2),
+(4, 2, 3),
+(1, 2, 4),
+(6, 2, 8),
+(9, 2, 11),
+(10, 2, 14),
+(11, 2, 15),
+(12, 2, 19),
+(13, 2, 20),
+(2, 3, 2);
